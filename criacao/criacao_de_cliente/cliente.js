8b507fd0a2224c0623e73fba95867826c701eca5
Gustavo <gustavohenrique6224@gmail.com>
2025-01-30 11:30:50 -0300

    adicionado API no formulário de cadastro de cliente para buscar o estado, a cidade, o bairro e a rua pelo CEP

--- criacao/criacao_de_cliente/cliente.js
+++ criacao/criacao_de_cliente/cliente.js
@@ -1,142 +1,49 @@
 const myHeaders = {
     "Content-Type": "application/json"
 }
-const selectState = document.querySelector("#select-state")
-async function insertStates() {
-    const states = await fetch(`http://localhost:3000/product/states`, {
-        method: 'GET',
-        headers: myHeaders
-    })
-    if (states.status === 200) {
-        const statesJSON = await states.json()
-        selectState.innerHTML = `<option value="0">Selecione o estado</option>` + statesJSON.map(state => `<option value="${state.id_estado}">${state.nome_estado}</option>`).join('')
-        selectState.addEventListener('change', function () {
-            const selectedStateId = event.target.value
-            if (selectedStateId >= 1) {
-                localStorage.setItem("@frentecaixa-userStateID", selectedStateId)
-                insertCities()
-            }
-            else {
-                selectState.value = localStorage.getItem("@frentecaixa-userStateID")
-                Toastify({
-                    text: `Você deve selecionar um estado`,
-                    duration: 3000,
-                    gravity: "top",
-                    position: "center",
-                    style: {
-                        background: "red",
-                    }
-                }).showToast();
-            }
-        })
-    }
-}
-const selectCity = document.querySelector("#select-city")
-async function insertCities () {
-    const stateId = localStorage.getItem("@frentecaixa-userStateID")
-        const dados = {
-            stateId
-        }
-        const dadosJson = JSON.stringify(dados)
-    const cities = await fetch(`http://localhost:3000/product/cities`, {
-        method: 'POST',
-        body: dadosJson,
-        headers: myHeaders
-    })
-    if (cities.status == 200) {
-        const citiesJSON = await cities.json()
-        selectCity.innerHTML = `<option value="0">Selecione o município</option>` + citiesJSON.map(city => `<option value="${city.id_municipio}">${city.nome_municipio}</option>`).join('')
-        selectCity.addEventListener("change", function () {
-            const selectedCityId = event.target.value
-            if (selectedCityId >= 1) {
-                localStorage.setItem("@frentecaixa-userCityID", selectedCityId)
-                insertHoods()
-            }
-            else{
-                selectCity.value = localStorage.getItem("@frentecaixa-userCityID")
-                Toastify({
-                    text: `Você deve selecionar uma cidade`,
-                    duration: 3000,
-                    gravity: "top",
-                    position: "center",
-                    style: {
-                        background: "red",
-                    }
-                }).showToast();
-            }
-        })
-    } else {
-        Toastify({
-            text: `Não há cidades cadastradas nesse estado`,
-            duration: 3000,
-            gravity: "top",
-            position: "center",
-            style: {
-                background: "red",
-            }
-        }).showToast();
-    }
-}
-const selectHood = document.querySelector("#select-hood")
-async function insertHoods() {
-    const cityId = localStorage.getItem("@frentecaixa-userCityID")
-        const dados = {
-            cityId
-        }
-        const dadosJson = JSON.stringify(dados)
-        try {
-            const response = await fetch(`http://localhost:3000/product/hoods`, {
-                method: 'POST',
-                body: dadosJson,
-                headers: myHeaders
-            })
-            console.log(response)
-            if (response.status === 200) {
-                const neighborhoodsJSON = await response.json();
-                selectHood.innerHTML = neighborhoodsJSON.map(hood => `<option value="${hood.id_bairro}">${hood.nome_bairro}</option>`).join('')
-            }
-            else {
-                Toastify({
-                    text: `Não há bairros cadastrados nessa cidade`,
-                    duration: 3000,
-                    gravity: "top",
-                    position: "center",
-                    style: { background: "red" },
-                }).showToast();
-            }
-        } catch (error) {
-            Toastify({
-                text: `Erro ao carregar bairros)`,
-                duration: 3000,
-                gravity: "top",
-                position: "center",
-                style: { background: "red" },
-            }).showToast();
-        }
-}
-insertStates()
 
+function buscarEndereco(cep) {
+    return fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.erro) {
+                throw new Error("CEP inválido!");
+            }
+            return data;
+        });
+}
 const submit = document.querySelector("#submit")
 submit.addEventListener("click", (event)=> {
     event.preventDefault()
-    const name = document.querySelector("#input-name").value
-    const code = document.querySelector("#input-code").value
-    const nature = document.querySelector("#select-nature").value
-    const cpfCnpj = document.querySelector("#input-cpf_cnpj").value
-    const rg = document.querySelector("#input-rg").value
-    const birth = document.querySelector("#input-birth").value
-    const state = document.querySelector("#select-state").value
-    const city = document.querySelector("#select-city").value
-    const hood = document.querySelector("#select-hood").value
-    const road = document.querySelector("#input-road").value
-    const houseNumber = document.querySelector("#input-house").value
-    const email = document.querySelector("#input-email").value
-    
-    const dados = {
-        name, code, nature, cpfCnpj, rg, birth, state, city, hood, road, houseNumber, email
-    }
-    const dadosJSON = JSON.stringify(dados)
-    cadastrar(dadosJSON)
+    const cep = document.querySelector("#input-cep").value
+    buscarEndereco(cep)
+        .then(endereco => {
+            const state = endereco.uf
+            const city = endereco.localidade
+            const cityId = endereco.ibge
+            const hood = endereco.bairro
+            const road = endereco.logradouro
+
+            const name = document.querySelector("#input-name").value
+            const code = document.querySelector("#input-code").value
+            const nature = document.querySelector("#select-nature").value
+            const cpfCnpj = document.querySelector("#input-cpf_cnpj").value
+            const rg = document.querySelector("#input-rg").value
+            const birth = document.querySelector("#input-birth").value
+            const houseNumber = document.querySelector("#input-house").value
+            const email = document.querySelector("#input-email").value
+
+            console.log(state)
+            console.log(cityId)
+            console.log(hood)
+            console.log(road)
+            const dados = {
+                name, code, nature, cpfCnpj, rg, birth, state, cityId, hood, road, houseNumber, email
+            }
+            const dadosJSON = JSON.stringify(dados)
+            cadastrar(dadosJSON)
+        })
+        .catch(error => console.error("Erro ao buscar CEP:", error));
 })
 async function cadastrar(dados) {
     try {
@@ -146,8 +53,13 @@ async function cadastrar(dados) {
             headers: myHeaders
         })
         if (response.status === 200) {
-            const neighborhoodsJSON = await response.json();
-            selectHood.innerHTML = neighborhoodsJSON.map(hood => `<option value="${hood.id_bairro}">${hood.nome_bairro}</option>`).join('')
+            Toastify({
+                text: `Cadastro realizado com sucesso!`,
+                duration: 3000,
+                gravity: "top",
+                position: "center",
+                style: { background: "blue" },
+            }).showToast();
         }
         else {
             Toastify({
@@ -159,6 +71,7 @@ async function cadastrar(dados) {
             }).showToast();
         }
     } catch (error) {
+        console.log(error)
         Toastify({
             text: `Erro ao se conectar com a api`,
             duration: 3000,
