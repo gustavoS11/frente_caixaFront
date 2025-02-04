9ebed76a7681e41659b7269af663365863afff11
Gustavo <gustavohenrique6224@gmail.com>
2025-02-03 21:49:04 -0300

    iniciado criação dos pacotes de promoção para clientes

---
+++ criacao/criacao_de_pacote_de_preco/pacote_de_preco.js
@@ -0,0 +1,7 @@
+const myHeaders = {
+    "Content-Type": "application/json"
+}
+const submit = document.querySelector("#input-submit")
+
+const selectName = document.querySelector("#select-name")
+const selectProduct = document.querySelector("#select-value")
